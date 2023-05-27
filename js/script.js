97f0c22a7c917f058619344a745a9b4d89a18ed5
Advaita <vedanta3000@gmail.com>
2023-05-28 00:48:04 +0530

    added nodemon server

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const socket = io('http://localhost:8000');
+
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.getElementById('.container');
+
+const naam = prompt("Enter you name to join");
+socket.emit('new-user-joined', naam);
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -4,3 +4,15 @@ const io = require('socket.io')(8000)
 
 const users ={};
 
+io.on('connection',socket =>{
+    socket.on('new-user-joined', naam =>{
+        console.log("New user", naam)
+        users[socket.id] = naam;
+        socket.broadcast.emit('user-joined', naam);
+    });
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('recieve', {message: message, naam: users[socket.id]})
+    });
+})
+
