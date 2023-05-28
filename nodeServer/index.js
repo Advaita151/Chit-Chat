248f85070a46277a25b6d774eca68363f0b7ae96
Advaita <vedanta3000@gmail.com>
2023-05-28 15:52:41 +0530

    fixed server

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -9,22 +9,6 @@ app.use(express.json())
 app.use(cors())
 require('dotenv').config()
 
-/*const port = process.env.PORT || 5000;
-const uri = process.env.ATLAS_URI;
-
-app.get("/", (req,res) => {
-    res.send("Welcome")
-})
-
-app.listen(port, (req,res) =>{
-    console.log(`Server is running on port: ${port}`)
-})
-
-mongoose.connect(uri, {
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-}).then(() => console.log("MongoDb connection established")).catch((error) => console.log("MongodB connection failed: ", error.message))*/
-
 
 const users ={};
 
