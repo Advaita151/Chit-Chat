187936daca2d67c260ac31c08433558e9853c264
Advaita <vedanta3000@gmail.com>
2023-05-28 11:37:00 +0530

    added socket server

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const socket = io('http://localhost:8000');
 
 
+
+
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.getElementById('.container');
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,8 +1,33 @@
 //This is js for socket.io
 
 const io = require('socket.io')(8000)
+const cors = require('cors')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+app.use(express.json())
+app.use(cors())
+require('dotenv').config()
+
+/*const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.get("/", (req,res) => {
+    res.send("Welcome")
+})
+
+app.listen(port, (req,res) =>{
+    console.log(`Server is running on port: ${port}`)
+})
+
+mongoose.connect(uri, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(() => console.log("MongoDb connection established")).catch((error) => console.log("MongodB connection failed: ", error.message))*/
+
 
 const users ={};
+
 
 io.on('connection',socket =>{
     socket.on('new-user-joined', naam =>{
